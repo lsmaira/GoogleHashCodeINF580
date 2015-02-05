7f2341a8c71626f0c25eee589fab11616c854e66
cesarilharco <cesar.ilharco@gmail.com>
2015-02-05 16:52:24 +0100

    reader class, falta verificar ainda se tá tudo certo

--- src/Reader.java
+++ src/Reader.java
@@ -1,19 +1,48 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 class Reader {
 	
-	final int n;
-	final int m;
-	final int maxT;
-	final int c;
-	final Node initial;
+	static int n, m, maxT, c;
 	
-	final Car[] cars;
+	static ArrayList<Node> junctions;
+	static ArrayList<Car> cars;
 	
-	public Reader() {
-		n = m = maxT = c = 0;
-		initial = null;
-		cars = null;
-		// TODO: read from filef
+	public Reader(String fileName) throws FileNotFoundException {
+		
+		File data = new File(fileName);
+		Scanner sc = new Scanner(data);
+		
+		n = sc.nextInt();
+		m = sc.nextInt();
+		maxT = sc.nextInt();
+		c = sc.nextInt();
+		
+		//Create, read and store the junction list
+		junctions = new ArrayList<Node>();
+		for (int i=0; i<n; i++){
+			junctions.add(new Node (sc.nextInt(), sc.nextDouble(), sc.nextDouble()));
+		}
+		
+		//Read and store the streets
+		for (int i=0; i<m; i++){
+			Node orig = junctions.get(sc.nextInt());
+			Node dest = junctions.get(sc.nextInt());
+			int dir = sc.nextInt();
+			int cost = sc.nextInt();
+			int gain = sc.nextInt();
+			Street newStreet = new Street (i, orig, dest, dir, cost, gain);
+			orig.neighbors.put(dest, newStreet);
+			if (dir == 2){
+				dest.neighbors.put(orig, newStreet);
+			}
+			
+		}
+
+		sc.close();
 	}
 
 }
