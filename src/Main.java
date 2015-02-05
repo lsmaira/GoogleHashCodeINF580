f67f11012b7163b1b7ea0a13c40e2c53f9f8339c
cesarilharco <cesar.ilharco@gmail.com>
2015-02-05 17:59:30 +0100

    simbora

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,11 @@ public class Main {
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		Reader r = new Reader ("input.txt");
-		
+		for (int i=0; i<10; i++){
+			Node j = Reader.junctions.get(i);
+			System.out.print("Junction "+i+"\nNeighboors: ");
+			for (Node n : r.junctions.get)
+		}
 	}
 
 }
