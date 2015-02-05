41d5650c0e567dc4b129fdb653952607fd5c3a98
cesarilharco <cesar.ilharco@gmail.com>
2015-02-05 17:13:37 +0100

    main: reading file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.io.FileNotFoundException;
+
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		Reader r = new Reader ()
+		Reader r = new Reader ("input.txt");
+		
 	}
 
 }
