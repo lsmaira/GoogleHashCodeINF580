493965df5d25a4717f7d8770b2309e904aa463b1
LucasSmaira <lfsmaira@gmail.com>
2015-02-05 23:13:12 +0100

    Random

--- src/RandomSolver.java
+++ src/RandomSolver.java
@@ -1,23 +1,35 @@
+import java.io.FileNotFoundException;
 import java.util.Set;
 
 
 class RandomSolver {
 	
 	Reader reader;
-	final String fileName = "bla.txt";
+	final String fileName = "paris_54000.txt";
 	Set<Car> availableCars;
 	
 	public void main() {
-		reader = new Reader(fileName);
+		try {
+			reader = new Reader(fileName);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		for(Car car : reader.cars) {
 			availableCars.add(car);
 		}
 		
 		while(! availableCars.isEmpty()) {
 			for(Car car : availableCars) {
-				
+				for(Node neighbor : car.getPossibleMoves()) {
+					if(car.current.neighbors.get(neighbor).cost<car.remainingTime) {
+						car.goToNode(neighbor);
+						break;
+					}
+				}
 			}
 		}
+		
+		Writer writer = new Writer(reader.cars);
 	}
 
 }
--- src/Writer.java
+++ src/Writer.java
@@ -1,4 +1,12 @@
 
 class Writer {
 
+	public Writer(Car[] cars) {
+		
+	}
+	
+	public void generateSolution(String fileName) {
+		
+	}
+	
 }
