2cfd2ca848bd958a6fa409f947d1109c1dffdafc
LucasSmaira <lfsmaira@gmail.com>
2015-02-05 05:07:55 +0100

    Basic classes created

---
+++ src/Node.java
@@ -0,0 +1,12 @@
+
+class Node {
+
+	final double latitude;
+	final double longitude;
+	
+	public Node(double la, double lo) {
+		latitude = la;
+		longitude = lo;
+	}
+	
+}
--- src/Street.java
+++ src/Street.java
@@ -1,4 +1,18 @@
 
 class Street {
+	
+	Node orig;
+	Node dest;
+	int cost;
+	int gain;
+	boolean bothDirec;
+	
+	public Street(Node o, Node d, int c, int g, int dir) {
+		orig = o;
+		dest = d;
+		cost = c;
+		gain = g;
+		bothDirec = (dir==2);
+	}
 
 }
